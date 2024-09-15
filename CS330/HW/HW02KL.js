a3bf393de8b900112338f27a0f2d3d50f6b29c99
kaleah-l <kgleisher@gmail.com>
2024-09-14 22:41:48 -0400

    Update HW02KL.js

--- CS330/HW/HW02KL.js
+++ CS330/HW/HW02KL.js
@@ -16,11 +16,15 @@ function init()
     var sqrt = (Math.sqrt(3))/2;
     points=[
     vec2(-1.0,0.0),
-    vec2(1.0,0.0),
+    
     //
     vec2(-1/3,0.0),
     vec2(0.0, ((2/3)*sqrt)),
     vec2(1/3,0.0),
+    //
+
+    vec2(1.0,0.0),
+
     ];
     
     //
