c69a59359b4d3eaf6c39acc1c161a2b632fedc79
kaleah-l <kgleisher@gmail.com>
2024-09-17 10:31:17 -0400

    Update 2HW02KL.js

--- CS330/HW/2HW02KL.js
+++ CS330/HW/2HW02KL.js
@@ -15,6 +15,7 @@ function init()
     gl = canvas.getContext('webgl2');
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
+    var sqrt = (Math.sqrt(3))/2;
     points=[
         vec2(-1.0,0.0),
 
