55a2f9dce14e3c7fd96538af43e6360ccb2cff30
kaleah-l <kgleisher@gmail.com>
2024-09-17 16:02:22 -0400

    Update 2HW02KL.js

--- CS330/HW/2HW02KL.js
+++ CS330/HW/2HW02KL.js
@@ -14,6 +14,44 @@ function init()
     gl = canvas.getContext('webgl2');
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
+
+    
+    //  Configure WebGL
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+    //  Load shaders and initialize attribute buffers
+
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    // Load the data into the GPU
+
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+    // Associate out shader variables with our data buffer
+
+    var positionLoc = gl.getAttribLocation( program, "aPosition" );
+    gl.vertexAttribPointer( positionLoc, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( positionLoc );
+
+    document.getElementById("slider").onchange = function(event) {
+        sliderVal = parseInt(event.target.value);
+        render();
+    };
+
+    render();
+};
+
+function render() {
+    
+    var sliderVal = parseInt(document.getElementById("slider").value);
+
+    points=[];
+
     var sqrt = (Math.sqrt(3))/2;
     vertices=[
         vec2(-1.0,0.0),
@@ -80,42 +118,7 @@ function init()
     
         vec2(1.0,0.0)
     ];
-    
-    //  Configure WebGL
 
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-
-    //  Load shaders and initialize attribute buffers
-
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
-
-    // Load the data into the GPU
-
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
-
-    // Associate out shader variables with our data buffer
-
-    var positionLoc = gl.getAttribLocation( program, "aPosition" );
-    gl.vertexAttribPointer( positionLoc, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( positionLoc );
-
-    document.getElementById("slider").onchange = function(event) {
-        sliderVal = parseInt(event.target.value);
-        render();
-    };
-
-    render();
-};
-
-function render() {
-    
-    var sliderVal = parseInt(document.getElementById("slider").value);
-
-    points=[];
     if(sliderVal === 0) {
         points.push(vertices[0]);
         points.push(vertices[46]);
