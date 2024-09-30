84567ca085ef75731a33972aefeaa18ed5daf0e6
kaleah-l <kgleisher@gmail.com>
2024-09-29 20:18:09 -0400

    CS330

--- CS330/HW/HW03_6.js
+++ CS330/HW/HW03_6.js
@@ -1,35 +1,30 @@
-//Author: Kaleah leisher
-//Date 9/29/24
-
-var canvas;
+"use strict";
 var gl;
 
-var bufferId;
-var bufferIdA;
-var bufferIdB;
-
-var triangleA;
-var triangleB;
+var color = vec4(1.0,0.65,0.0,1.0);
+var colorA = vec4(1.0,0.65,0.0,1.0);
+var colorB = vec4(0.0,0.0,1.0,1.0);
+var colorLoc;
 
 var delay = 100;
-var toggleMorph = true;
+var morph = true;
 
-var deltaT = 0.01;
+var tParam=0.0;
 var tLoc;
-var tParam;
+var deltaT = 0.06;
 
 init();
 
 function init()
 {
-    canvas = document.getElementById("gl-canvas");
+    var canvas = document.getElementById("gl-canvas");
 
     gl = canvas.getContext('webgl2');
     if (!gl) alert("WebGL 2.0 isn't available");
 
     //defining shape arrays
 
-    triangleA = [
+    var triangleA = [
         vec2(-0.5,-0.6),
         //vec2(0,0.8),
         vec2(0,0.8),
@@ -38,7 +33,7 @@ function init()
         //vec2(-0.5,-0.6)
     ];
 
-    triangleB = [
+    var triangleB = [
         vec2(-0.2,-0.3),
         //vec2(0.3,0.4),
         vec2(0.3,0.4),
@@ -46,10 +41,6 @@ function init()
         vec2(0.8,-0.6),
         //vec2(-0.2,-0.3)
     ];
-
-    out = []
-
-    out=triangleA;
 
     //  Configure WebGL
 
@@ -62,69 +53,76 @@ function init()
     gl.useProgram(program);
 
     // Load the data into the GPU
-
-    bufferIdA = gl.createBuffer();
-    bufferIdB = gl.createBuffer();
+    var bufferIdA = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferIdA);
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufferIdB);
-    //gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(triangleA));
-    //gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(triangleB));
-
-    bufferId = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-    gl.bufferData(gl.ARRAY_BUFFER, 8*Math.pow(3, 6), gl.STATIC_DRAW);
-    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(out));
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(triangleA), gl.STATIC_DRAW);
 
     // Associate out shader variables with our data buffer
-
     var positionLocA = gl.getAttribLocation(program, "aPosition");
     gl.vertexAttribPointer(positionLocA, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(positionLocA);
 
+    // Load the data into the GPU
+    var bufferIdB = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferIdB);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(triangleB), gl.STATIC_DRAW);
+    
+    // Associate out shader variables with our data buffer
     var positionLocB = gl.getAttribLocation(program, "bPosition");
     gl.vertexAttribPointer(positionLocB, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(positionLocB);
 
-
     //from lab 7
     tLoc = gl.getUniformLocation( program, "t" );
-    //thetaLoc = gl.getUniformLocation( program, "uTheta" );
+
+    colorLoc = gl.getUniformLocation( program, "inColor" );
 
     // Toggle Button
-    var Btn = document.getElementById("toggleBtn");
-    Btn.addEventListener("click", toggleMorph);
+    //var Btn = document.getElementById("toggleBtn");
+    //Btn.addEventListener("click", toggleMorph);
+
+    // Initialize event handlers
+
+    document.getElementById("toggleBtn").onclick = function () {
+        morph = !morph;
+    };
 
     // Change morph on or off
-    function toggleMorph() {
-        if (toggleMorph == false){
-            toggleMorph == true;
-        } else {
-            toggleMorph == false;
-        }
-    }
+    // function toggleMorph() {
+    //     if (toggleMorph == false){
+    //         toggleMorph == true;
+    //     } else {
+    //         toggleMorph == false;
+    //     }
+    // }
 
     render();
 };
 
 function render()
 {
+
     gl.clear( gl.COLOR_BUFFER_BIT );
 
-    if (toggleMorph) {
-        tParam += deltaT;
-        if (tParam >= 1.0 || tParam <= 0.0) {
-            deltaT = -deltaT;
-        }
-    }
+    // if (toggleMorph) {
+    //     tParam += deltaT;
+    //     if (tParam >= 1.0 || tParam <= 0.0) {
+    //         deltaT = -deltaT;
+    //     }
+    // }
+
+    if (morph) tParam += deltaT;
+    if (tParam>=1.0 || tParam<= 0.0) deltaT = -deltaT;
+
+// just hardcoding the t value
+    //tParam = 0.0;
     gl.uniform1f(tLoc, tParam);
 
-    //use uniform to set and send t
-    //from lab 7
-    //theta += (rotation ? 0.1 : 0.0);
-    //gl.uniform1f(thetaLoc, theta);
+// set the color
+    color = mix(colorA,colorB,tParam);
+    gl.uniform4fv(colorLoc,color);
 
-    gl.drawArrays( gl.LINE_LOOP, 0, out.length);
-    //gl.drawArrays( gl.LINE_LOOP, 0, triangleB.length);
+    gl.drawArrays( gl.LINE_LOOP, 0, 3);
 
     setTimeout(
         function (){requestAnimationFrame(render);}, delay
