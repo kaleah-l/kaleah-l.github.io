7591c26fc6fd82e6a5c56ad75fab4ba4fb483480
kaleah-l <kgleisher@gmail.com>
2024-11-12 10:11:07 -0500

    CS330

--- CS330/Labs/Lab17TextureSettings.js
+++ CS330/Labs/Lab17TextureSettings.js
@@ -4,7 +4,8 @@ var points;
 
 var positionsArray = [];
 var colorsArray = [];
-var vertexColor = vec4( ,  ,  , 1.0);  
+var vertexColor = vec4( 1.0,  0.4,  0.0, 1.0);  
+var texSize = 64;
 
 window.onload = init;
 
@@ -17,9 +18,9 @@ function init()
 
 // isoceles triangle
     points=[
-    vec4(    ,    ,   , 1.0 ),
-    vec4(    ,    ,   , 1.0 ),
-    vec4(    ,    ,   , 1.0 )
+    vec4( -0.8, -0.8, 0.0, 1.0 ),
+    vec4(  0.0,  1.0, 0.0, 1.0 ),
+    vec4(  0.8, -0.8, 0.0, 1.0 )
     ];
 
     positionsArray.push(points[0]);
@@ -58,6 +59,50 @@ function init()
     var positionLoc =gl.getAttribLocation( program, "aPosition");
     gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(positionLoc);
+
+    // Create a checkerboard pattern using floats
+    var image1 = new Array()
+    for (var i =0; i<texSize; i++) image1[i] = new Array();
+    for (var i =0; i<texSize; i++)
+    for ( var j = 0; j < texSize; j++)
+    image1[i][j] = new Float32Array(4);
+    for (var i =0; i<texSize; i++) for (var j=0; j<texSize; j++) {
+    var c = (((i & 0x8) == 0) ^ ((j & 0x8) == 0));
+    image1[i][j] = [c, c, c, 1];
+    }
+
+    // Convert floats to ubytes for texture
+    var image2 = new Uint8Array(4*texSize*texSize);
+    for (var i = 0; i < texSize; i++)
+    for (var j = 0; j < texSize; j++)
+    for(var k =0; k<4; k++)
+    image2[4*texSize*i+4*j+k] = 255*image1[i][j][k];
+    var texCoordsArray = [];
+    
+    var texCoord = [
+    vec2(-0.8, -0.8),
+    vec2(0, 1),
+    vec2(0.8, -0.8)
+    ];
+
+    function configureTexture(image) {
+    var texture = gl.createTexture();
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, texSize, texSize, 0, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_LINEAR);
+    // gl.NEAREST_MIPMAP_NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST_MIPMAP_LINEAR);
+    }
+
+    texCoordsArray.push(texCoord[0]);
+    texCoordsArray.push(texCoord[1]);
+    texCoordsArray.push(texCoord[2]);
+
+
+
 
     // texture buffer
     var tBuffer = gl.createBuffer();
